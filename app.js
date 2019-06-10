5165ba9bfeaac88a4490f997b414e25c2e3572f5
Bartek Pacia <barpac02@gmail.com>
2019-06-10 21:14:25 +0200

    improved reading from console

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 const puppeteer = require("puppeteer");
-const readline = require("readline");
+const readline = require("readline-sync");
+const figlet = require("figlet");
 const $ = require("cheerio");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+console.log(figlet.textSync("MLB Thing\n"));
+
+const url = readline.question("Enter URL of the match: ");
+getHtml(url).then(html => {
+  process(html);
 });
 
-const matchUrl =
-  "https://www.mlb.com/gameday/d-backs-vs-dodgers/2019/03/29/565800#game_state=final,lock_state=final,game_tab=box,game=565800";
-
-rl.question("Siema Bartek, podaj url meczu: ", answer => {
-  getHtml(answer);
-});
-
-async function getHtml(url) {
+async function getHtml(_url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url);
+  await page.goto(_url);
   const content = await page.content();
-  // console.log(content);
+  console.log(`Browser: ${await browser.version()}`);
+  return content;
+}
 
+function process(_html) {
   const players = [];
-  const length = $("span.name > a", content).length;
-  console.log($("span.name > a", content).length);
+  const length = $("span.name > a", _html).length;
+  console.log(`${length} players in this match`);
 
   for (let i = 0; i < length; i++) {
-    // names.push($("span.name > a", content)[i].attribs.href);
-    const cheerio = $("span.name > a", content)[i];
+    const cheerio = $("span.name > a", _html)[i];
     const name = cheerio.attribs["aria-label"];
     const href = cheerio.attribs.href;
     players.push({
@@ -36,7 +34,22 @@ async function getHtml(url) {
     });
   }
 
+  players.sort(compareNames);
+
   players.forEach(player => {
     console.log(`${player.name} ${player.href}`);
   });
+
+  console.log("\n\nFinished. Press Ctrl + C to exit.");
+}
+
+function compareNames(a, b) {
+  const nameA = a.name;
+  const nameB = b.name;
+
+  let comparison = 0;
+  if (nameA > nameB) comparison = 1;
+  else if (nameA < nameB) comparison = -1;
+
+  return comparison;
 }
